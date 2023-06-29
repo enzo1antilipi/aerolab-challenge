b6d27e9fe26410da725deb047687b4050e94e313
enzo antilipi <enzoantilipi1@gmail.com>
2023-06-29 19:16:28 -0300

    primer commit

---
+++ pages/about.jsx
@@ -0,0 +1,21 @@
+import styles from "./components/Button.module.css";
+
+// const fectchPost = () => {
+//   return fetch(
+//     "https://private-cdebd-aerolabchallenge.apiary-mock.com/products"
+//   ).then((res) => res.json());
+// };
+// export default function about() {
+//   const posts = fectchPost();
+//   return (
+//     <section>
+//       {posts.map((post) => (
+//         <article key={post.id}>
+//           <h2>{post.name}</h2>
+//         </article>
+//       ))}
+//     </section>
+//     //  <div>
+//     //    <h1 className={styles.soy}>soy abouut</h1>     </div>
+//   );
+// }
--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,220 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import logo from "../public/logo.png";
+import tarjetas from "../public/tarjetas.png";
+import styles from "./components/Button.module.css";
+import tech from "../public/tech.png";
+import explore from "../public/explore.png";
+import Navbar from "./components/Navbar";
+import "bootstrap/dist/css/bootstrap.css";
+import { useEffect, useState } from "react";
+import DatosUser from "./components/DatosUser";
+import swal from "sweetalert";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+//a la variable mapa le asigno los valores de costo asi lo puedo trabajr como un arreglo
+//   array.map((p) => console.log(p));
+function Blog({ posts, posts2 }) {
+  const [arreglo, setArreglo] = useState(posts); //le paso el array de obejtos post a 'arreglo'
+  const [precio, setPrecio] = useState("");
+
+  //function mapear() {
+  const datosUsuario = [posts2];
+  const hadleMostrar = (e) => {
+    const array = [...arreglo]; //le asigno a la constante array el array de objetos
+    array.sort(function (a, b) {
+      return b.cost - a.cost;
+      //con .sort ordeno el array de objetos y con .cost accedo a la propiedad de costo del obejto para ordenarlos de forma numerica
+    });
+    setArreglo(array);
+    //array.map((p) => console.log(p));
+  };
+
+  const mostrarmenor = (e) => {
+    const array2 = [...arreglo];
+    array2.sort(function (a, b) {
+      return a.cost - b.cost;
+    });
+    setArreglo(array2);
+  };
+  const handleMostrarReciente = () => {
+    const arraydeReciente = [...posts];
+    setArreglo(arraydeReciente);
+  };
+
+  const handleFiltro = (ev) => {
+    const array3 = [...posts];
+
+    const resultado = array3.filter(
+      (array3) => array3.category === ev.target.value
+    );
+    if (resultado.length === 0) {
+      setArreglo(array3);
+    } else {
+      console.log(resultado);
+      setArreglo(resultado);
+    }
+    //lo que hago aqui es evaluar si el array resultado no tiene ningun elemento por que no encontro ninguna
+    //coincidencia entonces que me devuelva todos los articulos. esto serviria para la funcion del option "all products"
+  };
+  // aqui vamos a hacer que se reste el valor de puntos de los puntos del usuario
+
+  useEffect(() => {
+    const puntosinicial = datosUsuario["0"].points;
+    setPrecio(puntosinicial);
+  }, []);
+  const handlecanjear = (event) => {
+    const value = event.target.dataset.value;
+    const resultado = precio - value;
+    setPrecio(resultado);
+    swal(
+      "Felicitaciones",
+      "Canjeaste tu producto con exito, te quedan " + resultado + " puntos",
+      "success"
+    );
+  };
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div className={styles.contenedor2}>
+        {/* <ul>
+          <li>
+            <Link href="/dashboard/page">About Us</Link>
+          </li>
+        </ul>
+        esto es un ruta creada como recomienda nextjs
+        */}
+        <div className={styles.contenedordesplegable}>
+          <DatosUser datosUsuario={datosUsuario} precio={precio} />
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
+        <div className={styles.imagenesinicio}>
+          <div className={styles.letra}>
+            <Image src={explore} className={styles.explo} alt="explore" />
+            <Image src={tech} className={styles.tech} alt="tech" />
+            <div className={styles.contenedorlink}>
+              <a href="#productos" className={styles.verproductos}>
+                Ver prodcutos
+              </a>
+            </div>
+          </div>
+          <div className={styles.contenedor}>
+            <Image src={logo} className={styles.logo} alt="logo" />
+          </div>
+        </div>
+      </div>{" "}
+      <div className={styles.contenedorimagentarjetas}>
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          src={tarjetas}
+          className={styles.imagentarjetas}
+          alt="tarjetas"
         />
+      </div>{" "}
+      <div className={styles.tarjetas}>
+        <div>
+          <Navbar
+            handleMostrar={hadleMostrar}
+            mostrarmenor={mostrarmenor}
+            handleFiltro={handleFiltro}
+            handleMostrarReciente={handleMostrarReciente}
+          />
+          {/*  le estoy mandando las props al componente hijo el cual va a llamar a una funcion que esta aquie en el componente padre */}
+        </div>
+        <section id="productos">
+          <div className="row">
+            {/* el classname row me permite mostrar las carda como filashandleMostrar={hadleMostrar} */}
+            {arreglo.map(
+              (
+                post // EL slice me deja elejir cuantos elementos quiero mostrar
+              ) => (
+                <div className="col" key={post._id}>
+                  <div
+                    className="card"
+                    style={{
+                      width: "15rem",
+                      height: "17rem",
+                      margin: "0px",
+                      marginTop: "4px",
+                      borderRadius: "10px",
+                    }} // de esta manera le agrego estilos
+                  >
+                    <div
+                      className={styles.ima}
+                      style={{ borderRadius: "20px" }}
+                    >
+                      <img src={post.img.url} />
+                      <div className="card-body">
+                        <h5 className="card-title">{post.name}</h5>
+                        <h6 className="card-subtitle mb-2 text-body-secondary">
+                          {post.category}
+                        </h6>
+                        {/* <p className="card-text">Cost :{post.cost} points</p> */}
+                      </div>
+                    </div>
+                  </div>
+
+                  <div>
+                    {post.cost <= precio ? (
+                      <button
+                        className={styles.botonpuntos}
+                        data-value={post.cost}
+                        onClick={handlecanjear}
+                      >
+                        Canjear por {post.cost}
+                      </button>
+                    ) : (
+                      <button className={styles.botonNoCanje}>
+                        Necesitas {post.cost - precio} puntos
+                      </button>
+                    )}
+                  </div>
+                </div>
+              )
+            )}
+          </div>
+        </section>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </>
+  );
 }
+
+export async function getStaticProps() {
+  // Call an external API endpoint to get posts.
+  // You can use any data fetching library
+  const heders3 = { Accept: "application / json" };
+  const heders2 = { ContentType: "application/json" };
+  const headers = {
+    Authorization:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDUzM2YyMDA1YmY4YzAwMWE5ZTA4YjQiLCJpYXQiOjE2ODMxNzcyNDh9.vb7d_iIWr6jx0W8jlilucKuAYczgJrA856Lu75Kldxk",
+  };
+
+  const res = fetch(
+    "https://private-cdebd-aerolabchallenge.apiary-proxy.com/products",
+    {
+      headers,
+    }
+  );
+  const res2 = fetch(
+    "https://private-cdebd-aerolabchallenge.apiary-proxy.com/user/me",
+    {
+      headers,
+    }
+  );
+  const [response1, response2] = await Promise.all([res, res2]);
+
+  const [posts, posts2] = await Promise.all([
+    response1.json(),
+    response2.json(),
+  ]);
+
+  // By returning { props: { posts } }, the Blog component
+  // will receive `posts` as a prop at build time
+  // return <h1>hola</h1>;
+  return {
+    props: {
+      posts,
+      posts2,
+    },
+  };
+}
+
+export default Blog;
